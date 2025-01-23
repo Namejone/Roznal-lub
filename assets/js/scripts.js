ea79ef4726de6c5b6108cb92647524a23de65fba
namejone <devnamejone@gmail.com>
2025-01-23 19:40:17 +0600

    Book view page 1 Compleated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,80 @@
 const bar = document.querySelector('.bar');
 const sidebar = document.querySelector('.sidebar');
 
-// Toggle sidebar
-bar.addEventListener('click', () => {
-  sidebar.classList.toggle('show');
+if (bar) {
+  // Toggle sidebar
+  bar.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+  });
+
+  // Close sidebar when clicking outside of it
+  document.addEventListener('click', (event) => {
+    if (!sidebar.contains(event.target) && !bar.contains(event.target)) {
+      sidebar.classList.remove('show');
+    }
+  });
+}
+
+// library-category-slider
+var mySwiper = new Swiper('.category-slider', {
+  loop: true,
+  speed: 500,
+  autoplay: false,
+  navigation: {
+    nextEl: '.s-next',
+    prevEl: '.s-prev',
+  },
+  spaceBetween: 10,
+  slidesPerView: "auto",
+  breakpoints: {
+    768: {
+      spaceBetween: 10,
+    },
+    1024: {
+      spaceBetween: 10,
+    },
+  },
 });
 
-// Close sidebar when clicking outside of it
-document.addEventListener('click', (event) => {
-  if (!sidebar.contains(event.target) && !bar.contains(event.target)) {
-    sidebar.classList.remove('show');
-  }
+// Grade-slider
+var gradSlider = new Swiper('.grade-slider', {
+  loop: true,
+  speed: 500,
+  autoplay: false,
+  navigation: {
+    nextEl: '.g-next',
+    prevEl: '.g-prev',
+  },
+  spaceBetween: 10,
+  slidesPerView: "auto",
+  breakpoints: {
+    768: {
+      spaceBetween: 10,
+    },
+    1024: {
+      spaceBetween: 10,
+    },
+  },
 });
+
+// resourse-creator slider
+var gradSlider = new Swiper('.resourse-creator__slider', {
+  loop: true,
+  speed: 500,
+  autoplay: false,
+  navigation: {
+    nextEl: '.r-next',
+    prevEl: '.r-prev',
+  },
+  spaceBetween: 10,
+  slidesPerView: "auto",
+  breakpoints: {
+    768: {
+      spaceBetween: 10,
+    },
+    1024: {
+      spaceBetween: 10,
+    },
+  },
+});
+
